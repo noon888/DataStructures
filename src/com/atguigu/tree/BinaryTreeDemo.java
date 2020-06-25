b4a94ca8265da21eb6751fda9d458f2a499161ab
moon888 <3117219279@qq.com>
2020-06-25 11:21:32 +0800

    binaryTreeDemo

--- src/com/atguigu/tree/BinaryTreeDemo.java
+++ src/com/atguigu/tree/BinaryTreeDemo.java
@@ -1,5 +1,7 @@
 package com.atguigu.tree;
 
+import java.util.Objects;
+
 /**
  * @Author: Administrator
  * @DATE:2020/6/23 21:35
@@ -23,5 +25,161 @@ public class BinaryTreeDemo {
         binaryTree.midOrder();
         System.out.println();
         binaryTree.postOrder();
+//        System.out.println(binaryTree.preSearch(5));
+//        System.out.println(binaryTree.midSearch(4));
+        System.out.println(binaryTree.postSearch(5));
     }
 }
+class BinaryTree {
+    private Emp root;
+
+    public BinaryTree(Emp root) {
+        this.root = root;
+    }
+
+    public void preOrder () {
+        if (Objects.nonNull(root)) {
+            root.preOrder();
+        }
+    }
+
+    public void midOrder () {
+        if (Objects.nonNull(root)) {
+            root.midOrder();
+        }
+    }
+
+    public void postOrder () {
+        if (Objects.nonNull(root)) {
+            root.postOrder();
+        }
+    }
+
+    public Emp preSearch (int id) {
+        return root.preSearch(id);
+    }
+
+    public Emp midSearch(int id) {
+        return root.midSearch(id);
+    }
+
+    public Emp postSearch(int id) {
+        return root.postSearch(id);
+    }
+}
+
+class Emp {
+    private final int id;
+    private final String name;
+    private Emp left;
+    private Emp right;
+
+    public Emp(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void preOrder () {
+        System.out.print(this.id + " ");
+        if (Objects.nonNull(this.left)) {
+            this.left.preOrder();
+        }
+        if (Objects.nonNull(this.right)) {
+            this.right.preOrder();
+        }
+    }
+
+    public void midOrder () {
+        if (Objects.nonNull(this.left)) {
+            this.left.midOrder();
+        }
+        System.out.print(this.id + " ");
+        if (Objects.nonNull(this.right)) {
+            this.right.midOrder();
+        }
+    }
+
+    public void postOrder () {
+        if (Objects.nonNull(this.left)) {
+            this.left.postOrder();
+        }
+        if (Objects.nonNull(this.right)) {
+            this.right.postOrder();
+        }
+        System.out.print(this.id + " ");
+    }
+
+    public void setLeft(Emp left) {
+        this.left = left;
+    }
+
+    public void setRight(Emp right) {
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "Emp{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public Emp preSearch (int id) {
+        System.out.println("统计查找次数，放在id比较前面");
+        if (this.id == id) {
+            return this;
+        }
+        Emp emp = null;
+        if (this.left != null) {
+            emp = this.left.preSearch(id);
+        }
+        if (Objects.nonNull(emp)) {
+            return emp;
+        }
+        if (this.right != null) {
+            return this.right.preSearch(id);
+        }
+        return null;
+    }
+
+    public Emp midSearch(int id) {
+        Emp emp = null;
+        if (Objects.nonNull(this.left)) {
+            emp = this.left.midSearch(id);
+        }
+        if (Objects.nonNull(emp)) {
+            return emp;
+        }
+        System.out.println("统计查找次数，放在id比较前面");
+        if (this.id == id) {
+            return this;
+        }
+        if (Objects.nonNull(this.right)) {
+            return this.right.midSearch(id);
+        }
+        return null;
+    }
+
+    public Emp postSearch(int id) {
+        Emp emp = null;
+        if (Objects.nonNull(this.left)) {
+            emp = this.left.postSearch(id);
+        }
+        if (Objects.nonNull(emp)) {
+            return emp;
+        }
+        if (Objects.nonNull(this.right)) {
+            emp = this.right.postSearch(id);
+        }
+        if (Objects.nonNull(emp)) {
+            return emp;
+        }
+        System.out.println("统计查找次数，放在id比较前面");
+        if (this.id == id) {
+            return this;
+        }
+        return null;
+    }
+}
+
